b7fec3a0dcd1f9b20fdc2ba7bd71daf216276eaa
Lorca Karen Nicole <karennicolelorca06gmail.com>
2026-06-29 21:58:26 -0300

    Gestion de Persistencia con Entity Framework 10

--- Dsw2026Ej15.Api/Dsw2026Ej15.Api/Program.cs
+++ Dsw2026Ej15.Api/Dsw2026Ej15.Api/Program.cs
@@ -2,6 +2,7 @@
 using Dsw2026Ej15.Api.Middleware;
 using Dsw2026Ej15.Data;
 using Dsw2026Ej15.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dsw2026Ej15.Api
 {
@@ -11,12 +12,16 @@ namespace Dsw2026Ej15.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
             // Add services to the container.
 
-
+            builder.Services.AddDbContext<Dsw2026Ej15DbContext>(options=>
+            {
+                options.UseSqlServer(connectionString);
+            });
             builder.Services.AddControllers();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddSingleton<IPersistence, PersistenceInMemory>();
+            builder.Services.AddScoped<IPersistence, PersistenceEf>();
             builder.Services.AddHealthChecks();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             //builder.Services.AddOpenApi();
@@ -37,6 +42,11 @@ namespace Dsw2026Ej15.Api
             app.MapControllers();
 
             app.MapHealthChecks("/health-check");
+
+            using var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<Dsw2026Ej15DbContext>();
+            context.SeedworkSpecialities(@"specialities.json");
 
             app.Run();
         }
--- Dsw2026Ej15.Api/Dsw2026Ej15.Domain/Entities/Speciality.cs
+++ Dsw2026Ej15.Api/Dsw2026Ej15.Domain/Entities/Speciality.cs
@@ -4,11 +4,13 @@ using System.Text;
 
 namespace Dsw2026Ej15.Domain.Entities;
 
-    public class Speciality : BaseEntity
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-    public Speciality(string name, string description,Guid? id = null) : base(id)
+public class Speciality : BaseEntity
+{
+    public string Name { get; init; }
+    public string Description { get; init; }
+
+    private Speciality() { }
+    public Speciality(string name, string description, Guid? id = null) : base(id)
     {
         Name = name;
         Description = description;
@@ -16,4 +18,4 @@ namespace Dsw2026Ej15.Domain.Entities;
     }
 
 }
-    
+
